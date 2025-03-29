ced0501a9be91a843d326d9fef20ec60e5563894
TeejMcSteez <tjhall047@gmail.com>
2025-03-29 12:58:58 -0400

    Minor UI Update to projects and Blog Posts

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -4,6 +4,22 @@ import BackToTop from "./BackToTop";
 import { useScroll } from "@/hooks/useScroll";
 import ReactMarkdown from "react-markdown";
 
+const CustomH1 = ({ children }) => {
+    return <h1 className="text-2xl font-bold">{children}</h1>;
+};
+
+const CustomH2 = ({ children }) => {
+    return <h2 className="text-xl font-semibold">{children}</h2>
+};
+
+const CustomH3 = ({ children }) => {
+    return <h3 className="font-medium underline">{children}</h3>
+};
+
+const CustomP = ({ children }) => {
+    return <p className="text-neutral-900 dark:text-neutral-100 leading-relaxed">{children}</p>;
+};
+
 export default function BlogPost({ blogContent }) {
     const topRef = useRef(null);
     const { y } = useScroll();
@@ -11,12 +27,20 @@ export default function BlogPost({ blogContent }) {
 
     const data = blogContent.data;
     const info = blogContent.file;
+
+    const defaultComponents = ReactMarkdown.defaultProps?.components || {};
+    const mergedComponents = { ...defaultComponents, h1: CustomH1, p: CustomP, h2: CustomH2, h3: CustomH3};
+
     return (
         <div className="flex flex-col items-center justify-center bg-neutral-200 dark:bg-zinc-800 p-4" ref={topRef}>
-        <p className="text-center ">Added: {data.date ? data.date : "No Date In Metadata"}</p>
-        <div className="flex flex-col justify-center items-center bg-neutral-100 dark:bg-zinc-900 p-4 shadow-md hover:shadow-xl transition-shadow duration-300 rounded-sm m-2">
-            <ReactMarkdown>{info}</ReactMarkdown>
-        </div>
+            <p className="text-center">
+                Added: {data.date ? data.date : "No Date In Metadata"}
+            </p>
+            <div className="flex flex-col justify-center items-center bg-neutral-100 dark:bg-zinc-900 p-4 shadow-md hover:shadow-xl transition-shadow duration-300 rounded-sm m-2">
+                <ReactMarkdown components={mergedComponents}>
+                    {info}
+                </ReactMarkdown>
+            </div>
             {isScrolled ? <BackToTop targetRef={topRef}/> : null}
         </div>
     );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -31,7 +31,7 @@ export default function Blog() {
     return (
         <>
         <div className='dark:text-white flex flex-col place-items-center'>
-            <h1 className='text-4xl m-2 p-2'>GitHub Stream</h1>
+            <h1 className='text-4xl m-2 p-2'>GitHub Public Repo Stream</h1>
                 <div className='grid md:grid-cols-3 grid-flow-dense gap-2'>
                     {data.map((repo) => (
                         <div key={repo.full_name} className='bg-neutral-200 dark:bg-neutral-800 m-4 p-2 rounded-lg text-balance text-pretty'>
