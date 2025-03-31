aecb87c62c9ca6a9b00805e5de8507272c995291
TeejMcSteez <tjhall047@gmail.com>
2025-03-30 21:11:53 -0400

    Minor update to back to top button on landing page fixed ref target issue by making server page client (heavy handed I know)

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,28 @@
+"use client";
+
 import Landing from "@/components/Landing";
 import NavBar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import BackToTop from "@/components/BackToTop";
+
+import { useScroll } from "@/hooks/useScroll";
+import { useRef } from "react";
 
 export default function Home() {
+  const { y } = useScroll();
+  const isScrolled = y > 100;
+  const topRef = useRef(null);
 
   return (
     <>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen" ref={topRef}>
         <NavBar />
         <main className="flex flex-grow items-center justify-center mt-20 md:mt-1">
           <Landing />
         </main>
         <Footer />
       </div>
+      {isScrolled ? <BackToTop targetRef={topRef} /> : null}
     </>
   );
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,18 +2,15 @@
 
 import { useRef } from "react";
 import Skills from "./Skills";
-import { useScroll } from "@/hooks/useScroll";
-import BackToTop from "./BackToTop";
+
 
 
 export default function Landing() {
-    const { y } = useScroll();
-    const topRef = useRef(null);
-    const isScrolled = y > 100;
+
 
     return (
         <>
-        <div className="flex flex-col items-center justify-center" ref={topRef}>
+        <div className="flex flex-col items-center justify-center">
         <div className='flex flex-col text-center dark:bg-zinc-900 bg-neutral-100 rounded-xl shadow-lg p-10 m-4'>
             <h2 className="text-xl font-medium mb-2">Hello, I'm Tommy Hall!</h2>
             <br />
@@ -26,7 +23,6 @@ export default function Landing() {
             <Skills />  
         </div>
         </div>
-        {isScrolled ? <BackToTop targetRef={topRef} /> : ""}
         </>
     );
 }
