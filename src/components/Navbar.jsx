279bbc04ba2865d70082de733a726a54f492197b
TeejMcSteez <tjhall047@gmail.com>
2025-04-01 23:25:13 -0400

    Updated navbar and skill centering for images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 
 export default function NavBar() {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(true);
 
     function toggleOpen() {
         setIsOpen(!isOpen);
@@ -13,14 +13,16 @@ export default function NavBar() {
         if (isOpen) {
             return null;
         } else {
-            return <a href='#' className='dark:text-white text-black'>MENU ☰</a>;
+            return (
+                <a href='#' className='dark:text-white text-black w-1/4 h-1/4 md:hidden'><img src='/assets/menu.svg' className='ml-auto w-1/2 h-1/2 p-1 m-1 dark:bg-white dark:rounded-4xl' onClick={toggleOpen}/></a>
+            );
         }
     }
 
     return (
         <>
         <header className='flex justify-between bg-neutral-100 dark:bg-zinc-900 p-2'>
-            <h1 className='m-5 text-2xl'>Tommy Halls Portfolio</h1>
+            <h1 className='m-5 text-2xl text-start items-start'>Tommy Halls Portfolio</h1>
 
             <nav className='hidden md:block m-5 text-white'>
                 <a href="/" className="m-2 px-3 py-2 bg-red-600 rounded hover:bg-red-800">Home</a>
@@ -31,9 +33,7 @@ export default function NavBar() {
 
             </nav>
 
-            <button className='md:hidden text-white m-2 p-2 rounded-xl dark:bg-zinc-900' onClick={toggleOpen} >
-                {buttonState()}
-            </button>
+            {buttonState()}
 
         </header>
         <div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Masonry from "@mui/lab/Masonry";
-import { ImageList, ImageListItem } from "@mui/material";
-import { useState, useRef } from "react";
+import { useState } from "react";
 const imgs = [
     {src: "/assets/c++.svg", alt:"C++", ex: "https://github.com/TeejMcSteez/ESPTempSensor"},
     {src: "/assets/java.svg", alt:"Java", ex: "https://github.com/TeejMcSteez/stockChartsWebScraper"},
@@ -22,11 +20,13 @@ export default function Skills() {
     return (
         <section className="relative overflow-hidden py-20">
             <div className="container mx-auto px-4">
-                <div className="flex flex-col items-center justify-center space-y-4">
-                    <div className="container flex flex-col items-center">
-                        <a href={imgs[index].ex}><img src={imgs[index].src} alt={imgs[index].alt}/></a>
+                <div className="flex flex-col items-center justify-center space-y-8">
+                    <div className="container flex flex-col items-center justify-center">
+                        <a className="self-center justify-center items-center" href={imgs[index].ex}><img src={imgs[index].src} alt={imgs[index].alt}/></a>
                     </div>
-                    <a href="#" className="flex flex-col items-center" onClick={handleClick}><img src="/assets/button.svg" className="md:w-1/2 md:h-1/2 w-1/4 h-1/4"/></a>
+                    <a href="#" className="flex flex-col items-center w-16 hover:scale-110 transition-transform" onClick={handleClick}>
+                        <img src="/assets/button.svg" className="w-full h-full"/>
+                    </a>
                 </div>
             </div>
         </section>
