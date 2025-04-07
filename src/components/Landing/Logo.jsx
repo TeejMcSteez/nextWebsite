1f166ceab9d4ee3a59739ca1e71be8afc614ae22
TeejMcSteez <tjhall047@gmail.com>
2025-04-07 13:47:38 -0400

    increased speed at which the logo is written

--- src/components/Landing/Logo.jsx
+++ src/components/Landing/Logo.jsx
@@ -1,9 +1,11 @@
 import Typewriter from "@/components/About/Typewriter";
 
 // Basic typewriter effect for the logo text
+// Still don't know if I like this or not
+// I might just go with a simple text logo instead
 export default function Logo() {
     const text = "Tommy Hall's Portfolio";
-    const speed = 60;
+    const speed = 10;
 
     return (
         <div className="flex text-2xl bold m-1 p-2">
