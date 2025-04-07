54bd6d3de70d5d6ea0daeb0a3e49605827a6261c
TeejMcSteez <tjhall047@gmail.com>
2025-04-07 00:01:56 -0400

    Added typewrite to logo

--- src/components/Landing/Logo.jsx
+++ src/components/Landing/Logo.jsx
@@ -1,7 +1,27 @@
+
+import { useState, useEffect } from "react";
+
+// Basic typewriter effect for the logo text
 export default function Logo() {
+    const text = "Tommy Hall's Portfolio";
+    const speed = 58;
+    const [displayedText, setDisplayedText] = useState("");
+
+    useEffect(() => {
+        let index = 0;
+        const interval = setInterval(() => {
+            if (index < text.length) {
+                setDisplayedText(text.slice(0, index + 1));
+                index++;
+            } else {
+                clearInterval(interval);
+            }
+        }, speed);
+
+        return () => clearInterval(interval); // cleanup onUnmount
+    }, [text, speed]);
+
     return (
-        <div className="">
-            <h1 className='m-5 text-2xl text-start items-start'>Tommy Halls Portfolio</h1>
-        </div>
+        <h1 className="flex flex-row m-1 p-2 text-2xl bold">{displayedText}</h1>
     );
 }
