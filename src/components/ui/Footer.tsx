bda6b7577c4387918865cc65b02a16370a19aba1
TeejMcSteez <tjhall047@gmail.com>
2025-04-11 00:13:36 -0400

    Added svgs instead of text for footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
     return (
@@ -6,14 +7,23 @@ export default function Footer() {
             <div className="max-w-4xl mx-auto px-4">
                 <div className="flex flex-col items-center gap-4">
                     <div className="flex gap-6">
-                        <Link href="https://github.com/TeejMcSteez" className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white transition-colors">
-                            Github
+                        <Link href="https://github.com/TeejMcSteez" className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white transition-colors hover:scale-125">
+                            <Image height={25} width={25} 
+                                src={"/assets/github-mark-white.svg"} 
+                                alt="GitHub Logo"
+                            />
                         </Link>
-                        <Link href="/contact" className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white transition-colors">
-                            Contact
+                        <Link href="/contact" className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white transition-colors invert hover:scale-125">
+                            <Image height={25} width={25}
+                                src={"/assets/contact.svg"}
+                                alt="Contact Logo"
+                            />
                         </Link>
-                        <Link href="/blog" className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white transition-colors">
-                            Blog
+                        <Link href="/blog" className="text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white transition-colors invert hover:scale-125">
+                            <Image height={25} width={25}
+                                src={"/assets/blog.svg"}
+                                alt="Blog Logo"
+                            />
                         </Link>
                     </div>
                     <div className="text-xs text-gray-500 dark:text-gray-400">
